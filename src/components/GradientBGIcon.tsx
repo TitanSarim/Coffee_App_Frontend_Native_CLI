c7c63be699a7ad0fe16f5f47400ba02fc5af8316
TitanSarim <muhammadsarim203@gmail.com>
2023-12-10 23:29:18 +0500

    home screen

---
+++ src/components/GradientBGIcon.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import { COLORS, SPACING } from '../theme/theme';
+import CustomIcon from './CustomIcon';
+
+interface GradientBGIcon {
+    name: string;
+    color: string;
+    size: number
+}
+
+
+const GradientBGIcon: React.FC<GradientBGIcon> = ({name, color, size}) => {
+  return (
+    <View style={styles.Container}>
+      <LinearGradient
+        start={{x: 0,  y: 0}}
+        end={{x: 1, y: 1}}
+        colors={[COLORS.primaryGreyHex, COLORS.primaryBlackHex]}
+        style={styles.LinearGradientBg}>
+            <CustomIcon name={name} color={color} size={size}/>
+        </LinearGradient>
+    </View>
+  );
+};
+
+export default GradientBGIcon;
+
+const styles = StyleSheet.create({
+    Container:{
+        borderWidth: 2,
+        borderColor: COLORS.secondaryDarkGreyHex,
+        borderRadius: SPACING.space_12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: COLORS.secondaryDarkGreyHex,
+        overflow: 'hidden',
+    },
+
+    LinearGradientBg: {
+        height: SPACING.space_36,
+        width: SPACING.space_36,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
